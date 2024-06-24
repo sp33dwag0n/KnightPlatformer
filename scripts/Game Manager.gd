c4b3023f7a323f3901f6c06e4780636aa3d10477
sp33dwag0n <bryancui.111@gmail.com>
2024-06-24 15:25:06 -0400

    made level larger

--- scripts/Game Manager.gd
+++ scripts/Game Manager.gd
@@ -3,15 +3,16 @@ extends Node
 @onready var score_label = $ScoreLabel
 @onready var message = $Message
 
+const COINS = 17
 var score = 0
 var input = true
 
 func add_score():
 	score += 1
-	if score == 10:
+	if score == COINS:
 		message.text = "Naisu!"
 	else:
 		message.text = "Mada Mada!"
 	
-	score_label.text = "You collected\n" + str(score) + " out of 10 coins!"
+	score_label.text = "You collected\n" + str(score) + " out of " + str(COINS) + " coins!"
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -15,10 +15,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if global.input and Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
@@ -30,6 +26,10 @@ func _physics_process(delta):
 		elif velocity.x < 0:
 			velocity.x = min(velocity.x - DECELERATION * delta, 0)
 	else:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
 		if direction == 1:
 			animated_sprite.flip_h = false
 		elif direction == -1:
