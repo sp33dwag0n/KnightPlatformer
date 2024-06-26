4fa263e43dfc0ae94a1188d639b5c938fe7c8a4c
sp33dwag0n <bryancui.111@gmail.com>
2024-06-26 10:05:19 -0400

    fixed coins message at 0 coins and autodetects number of coins

--- scripts/Game Manager.gd
+++ scripts/Game Manager.gd
@@ -2,17 +2,22 @@ extends Node
 
 @onready var score_label = $ScoreLabel
 @onready var message = $Message
+@onready var coins = $"../Coins"
 
-const COINS = 18
+
+var coin_count = 0
 var score = 0
 var input = true
 
+func _ready():
+	coin_count = coins.get_child_count()
+	message.text = "Mada Mada!"
+	score_label.text = "You collected\n0 out of " + str(coin_count) + " coins!"
+
 func add_score():
 	score += 1
-	if score == COINS:
+	if score == coin_count:
 		message.text = "Naisu!"
-	else:
-		message.text = "Mada Mada!"
 	
-	score_label.text = "You collected\n" + str(score) + " out of " + str(COINS) + " coins!"
+	score_label.text = "You collected\n" + str(score) + " out of " + str(coin_count) + " coins!"
 	
